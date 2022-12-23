06467b76e80895971f54e54585e0e4f9597b95a4
Andrea Morello <andrea.morello@greenext.it>
2022-12-23 11:28:59 +0100

    setting middlewares and services register startup

---
+++ src/FoxBeTestA.Api/FoxBeTestA.Api/RegisterStartupMiddlewares.cs
@@ -0,0 +1,17 @@
+﻿public static class RegisterStartupMiddlewares
+{
+    public static WebApplication SetupMiddlewares(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.UseHttpsRedirection();
+
+        app.MapControllers();
+
+        app.Run();
+
+        return app;
+    }
+}
+
